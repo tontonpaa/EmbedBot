723dbbae20831e9a5d2804d1390abe6ed35b8d5e
tontonpaa <nktk.20202004@gmail.com>
2025-04-08 18:37:43 +0900

    main.pyを更新

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ async def on_ready():
     activity = discord.CustomActivity(name="私は私、それだけ")
     await bot.change_presence(activity=activity)
     print(f'{bot.user.name} has connected to Discord!')
+    await bot.tree.sync() # グローバルコマンドを同期 (推奨)
 
 async def main():
     await act.setup(bot)
+    await EmbedCommands.setup(bot)  # EmbedCommands の setup 関数を呼び出して Cog を追加
     await bot.start(os.getenv('DISCORD_TOKEN'))
 
 if __name__ == '__main__':
